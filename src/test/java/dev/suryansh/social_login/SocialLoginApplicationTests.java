37edb42fbb49dc6d4db66eaf9d85a4d4c2a1cc98
Suryansh Chaturvedi <suryansh.chaturvedi@stud.uni-heidelberg.de>
2024-01-19 10:23:13 +0100

    First Commit

---
+++ src/test/java/dev/suryansh/social_login/SocialLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.suryansh.social_login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
